6d721ff50cf31ddd26ebee0128ef3523db6b480d
PartumGames <37095216+PartumGames@users.noreply.github.com>
2018-06-07 10:45:16 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Camera cam;
+    public Transform hand;
+    Animator handAnim;
+    public float interactDist = 10f;
+
+    private void Start()
+    {
+        handAnim = hand.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        DoInteraction();
+    }
+
+    private void DoInteraction()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactDist))
+        {
+            if(hit.collider.tag == "Harvest")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Harvest currentHarvest = hit.collider.GetComponent<Harvest>();
+                    currentHarvest.HarvestResources(10f);
+                }
+            }
+        }
+    }
+
+}
